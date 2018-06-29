677b89137b149923099e00673204715bc05c469a
kiefergrylls <kiefer.coze@viacesi.fr>
2018-06-29 11:11:43 +0200

    Migrating EliotAuth view to DevicesActivity

--- app/src/main/java/com/atlantis/mobileapp/activities/DevicesActivity.java
+++ app/src/main/java/com/atlantis/mobileapp/activities/DevicesActivity.java
@@ -1,11 +1,15 @@
 package com.atlantis.mobileapp.activities;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
@@ -16,27 +20,51 @@ import com.atlantis.mobileapp.utilities.CustomAdapter;
 
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DevicesActivity extends AppCompatActivity {
+    final static String CLIENT_ID = "358ca400-fdf6-4357-8cca-27caa6699197";
+    final static String CLIENT_SECRET = "*d,|`89Jnx/Ea5O8y$T724W4";
+    final static String REDIRECT_URI = "https://login.microsoftonline.com/tfp/oauth2/nativeclient";
+    final static String KEY_CODE = "KEY_CODE";
+    final static String URL_AUTH = "https://partners-login.eliotbylegrand.com/authorize?client_id=" + CLIENT_ID + "&response_type=code&redirect_uri=" + REDIRECT_URI;
 
     public static final String KEY_USER = "KEY_USER";
     public static final String KEY_DEVICE = "KEY_DEVICE";
-    public static final String KEY_CODE = "KEY_CODE";
-
     //UI
     ListView listView_devices;
+    String token;
+    WebView webView;
+    ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_devices);
 
-        Intent i = getIntent();
-        String token = i.getStringExtra(KEY_CODE);
+        //Intent i = getIntent();
+        //String token = i.getStringExtra(KEY_CODE);
 
+        webView = (WebView) findViewById(R.id.webView_authBis);
+        listView_devices = (ListView)findViewById(R.id.listView_devices);
+
+
+        webView.setVisibility(View.VISIBLE);
+        listView_devices.setVisibility(View.GONE);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        startWebView();
         Log.d("access_token",":" + token);
         try {
             JSONObject jsonObject = new JSONObject(token);
@@ -50,10 +78,7 @@ public class DevicesActivity extends AppCompatActivity {
         } catch (Exception e) {
             Log.d("Exception", e.getMessage());
         }
-        String userId = i.getStringExtra(KEY_USER);
-
-        listView_devices = (ListView)findViewById(R.id.listView_devices);
-
+        //String userId = i.getStringExtra(KEY_USER);
         //TODO get user devices
 
         ArrayList<Device> array = new ArrayList<>();
@@ -74,6 +99,81 @@ public class DevicesActivity extends AppCompatActivity {
         });
     }
 
+    private void startWebView() {
+        webView.clearCache(true);
+        webView.clearHistory();
+        webView.clearFormData();
+        webView.setWebViewClient(new WebViewClient(){
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                String fragment = "?code=";
+                int start = url.indexOf(fragment);
+                if (start > -1) {
+                    webView.stopLoading();
+                    final String code = url.substring(start + fragment.length(), url.length());
+                    Log.d("CODE",code);
+
+                    dialog = ProgressDialog.show(DevicesActivity.this, "Loading","Connecting to Eliot account, please wait...", true);
+                    Thread t = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                getToken(code);
+                            } catch (MalformedURLException e) {
+                                Log.d("Exception", e.getMessage());
+                            }
+                        }
+                    });
+                    t.start();
+                }
+            }
+        });
+        webView.loadUrl(URL_AUTH);
+    }
+
+    private void getToken(String code) throws MalformedURLException {
+        URL url = new URL("https://partners-login.eliotbylegrand.com/token");
+        HttpURLConnection conn;
+        try {
+            String data = "client_id=" + URLEncoder.encode(CLIENT_ID, "UTF-8");
+            data += "&grant_type=" + URLEncoder.encode("authorization_code", "UTF-8");
+            data += "&code=" + URLEncoder.encode(code, "UTF-8");
+            data += "&client_secret=" + URLEncoder.encode(CLIENT_SECRET, "UTF-8");
+            conn = (HttpURLConnection)url.openConnection();
+            conn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.write(data);
+            wr.flush();
+            wr.close();
+            InputStream is = null;
+            try {
+                is = conn.getInputStream();
+            }
+            catch (IOException e) {
+            }
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            StringBuffer sb = new StringBuffer();
+            String line = null;
+            while((line = reader.readLine()) != null){
+                sb.append(line).append("\n");
+            }
+            reader.close();
+            token = sb.toString();
+            Log.d("D","ATA : " + token);
+        }  catch (IOException e) {
+            Log.d("Exception", e.getMessage());
+        }
+        dialog.cancel();
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                webView.setVisibility(View.GONE);
+                listView_devices.setVisibility(View.VISIBLE);
+            }
+        });
+//        Intent intent = new Intent(DevicesActivity.this,DevicesActivity.class);
+//        intent.putExtra(KEY_CODE,token);
+//        startActivity(intent);
+    }
 
     public static String decoded(String JWTEncoded) throws Exception {
         try {
